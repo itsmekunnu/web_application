233a526889494db3434d65e37194619b6b180048
Ashish Mishra <itsme.kunnu@gmail.com>
2015-01-24 16:41:12 +0530

    made changes that make the application more interactive

--- testerpage_reserve.php
+++ testerpage_reserve.php
@@ -6,36 +6,15 @@ if ($conn->connect_error)
 	}
 else
 	{ 
-		echo "Connected successfully";
+		//echo "Connected successfully";
 	}
-/*$sql = "CREATE DATABASE myDB3";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}*/
-// Create connection
 $conn = mysqli_connect("localhost","root","","myDB3");
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+
 }
 
-/* sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
-
-if (mysqli_query($conn, $sql)) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
-*/
 $v1=(string)$_POST["name1"];
 $v2=(string)$_POST["name2"];
 $v3=$_POST["mailid"];
@@ -50,6 +29,7 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 
-//mysqli_close($conn);
+<br/>
+echo "Thanks for registering";
 
 ?>
